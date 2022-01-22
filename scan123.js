81c2fe15bba93a1a91d8cd9ff66b9c8e62f93d80
aDerocher <am.derocher@gmail.com>
2022-01-22 11:02:52 -0800

    3 of 4 complete in js. so close. Wish I had time to fix and turn to PHP

--- scan123.js
+++ scan123.js
@@ -13,7 +13,8 @@ let makes = [
 ]
 let types = ['num', 'alph']
 let orders = ['asc', 'desc']
-
+// ==============================================================================================
+// =================================== Helper Functions =========================================
 // accepts a string
 // if string contains a word that starts with an integer, returns integer
 // otherwise returns false
@@ -47,7 +48,33 @@ function createObj(string, i){
     return obj
 }
 
-
+// sorts 2 strings alphabetically. if they're the same, returns false
+function alphabetical(s1, s2){
+    if (typeof s1 !== 'string' || typeof s2 !== 'string') {
+        console.log('Error: Function "alphabetical" only accepts strings as arguments')
+        return undefined
+    }
+    // if they're the same, just return false
+    if (s1 === s2){
+        return false
+    }
+    let sorted = [s1,s2].sort()
+    return sorted[0]
+}
+// sorts 2 strings numerically. if they're both undefined, returns false
+function numerical(s1, s2){
+    //if they are not undefined and they match
+    if (s1 === s2 && (s1 !== undefined)){
+        return false
+    }
+    //if both are valid (but do not match)
+    if(s1 || s2){
+        let sorted = [s1,s2].sort()
+        return sorted[0]
+    }
+}
+// ==============================================================================================
+// =================================== Main Function ============================================
 function dynamic_sort(arr, type, order){
     // base case
     if(arr.length <= 1){
@@ -67,62 +94,91 @@ function dynamic_sort(arr, type, order){
         // it may ease flexibility when adjusting for different orders based on the arguments
         let lesser;
         // according to the type...
-        if(type === 'num'){
-            if(iObj.year && pivot.year){
-                // find the lower year
-                if(iObj.year < pivot.year){
-                    lesser = true;
-                } else if (iObj.year >= pivot.year){ // <====== change this back to just 'greater than'
-                    lesser = false;
+        switch(type){
+            case 'num':
+                let sortedByYear = numerical(iObj.year, pivot.year)
+                if(sortedByYear){
+                    // if at least 1 of them has a valid year expressed
+                    // see if the lesser year is the object year
+                    lesser = (iObj.year === sortedByYear)
+                    break;
                 } else {
-                    let x = [iObj.make, pivot.make].sort()
-                    if(x[0] === iObj.make){ lesser = true }
-                    else { lesser = false}
+                    let sortedAlpha = alphabetical(iObj.make, pivot.make)
+                    // True edge case: have same make and year, so it doesnt matter. I dont think this can even happen
+                    if(!sortedAlpha){
+                        lesser = true
+                        break;
+                    } else {
+                        lesser = (sortedAlpha === iObj.make)
+                        break;
+                    }
                 }
-            } else if(iObj.year || pivot.year){
-                // if only one has year, it goes first (or second)
-                if(iObj.year){
-                    lesser = true
-                } else {
-                    lesser = false
-                }
-            } else {
-                // neither have year, so go alphabetically
-                let x = [iObj.make, pivot.make].sort()
-                if(x[0] === iObj.make){ lesser = true }
-                else { lesser = false}
-            }
-        }
-        // then to the order...
-        if(lesser === true){ lowers.push(item) }
-        else { highers.push(item) }
-    }
-    return dynamic_sort(lowers, type, order).concat([temp], dynamic_sort(highers, type, order))
-}
-// console.log(hasYear(makes[0]))
-// console.log(hasYear(makes[1]))
-// console.log(hasYear(makes[2]))
-// console.log(hasYear(makes[3]))
-// console.log(hasYear(makes[4]))
-console.log(dynamic_sort(makes, types[0], orders[0]));
-console.log(dynamic_sort(makes, types[0], orders[0]));
-console.log('=======================')
 
-// let y = ['allison','yolanda','alex','borris','netasha']
-// let z = [...y].shift()
-// console.log(typeof z === 'string')
-// console.log(z)
-// console.log(y)
-// console.log(['allison','yolanda','alex','borris','netasha'].sort())
-// console.log(y.sort(function(a,b){
-//     if(typeof a === 'string' && typeof b !== 'string'){
-//         console.log(a,b)
-//         return b-a
-//     }
-//     if(typeof a !== 'string' && typeof b === 'string'){
-//         console.log(a,b)
-//         return a-b
-//     } else {
-//         return a-b;
-//     }
-// }));
+            case 'alph':
+                // Go alphabetically
+                let sortedAlpha = alphabetical(iObj.make, pivot.make)
+                // edge case: have same make name. So go by Year
+                if(sortedAlpha){
+                    lesser = (sortedAlpha === iObj.make);
+                    break;
+                }
+                if(iObj.make === pivot.make){
+                    // if they have the same make name, sort it by year.
+                    let sortedByYear = numerical(iObj.year, pivot.year)
+                    if(sortedByYear){
+                    // if at least 1 of them has a valid year expressed
+                    // see if the lesser year is the object year
+                        lesser = (iObj.year === sortedByYear)
+                        break;
+                    } else {
+                        // true edge case. same make and year. this likely wont ever happen unless another error is going on
+                        lesser = true;
+                        break;
+                    }
+                }
+
+            default:
+                console.log('Error: not a valid sorting type');
+                return 'Error: not a valid sorting type';
+        }
+
+        // then to the order...
+        switch(order){
+            case 'asc':
+                if(lesser){ lowers.push(item) }
+                else { highers.push(item) }
+                break;
+            case 'desc':
+                if(!lesser){ highers.push(item) }
+                else { lowers.push(item) }
+                break;
+        }
+    }
+    // might have to remove buick from the array. it might be getting stuck as the 'permanent temp'
+    // might have to reverse the order of these depending on the type of order
+
+    switch(order){
+        case 'asc':
+            return [...dynamic_sort(lowers, type, order), temp, ...dynamic_sort(highers, type, order)]
+        case 'desc':
+            return [...dynamic_sort(highers, type, order), temp, ...dynamic_sort(lowers, type, order)]
+    }
+
+}
+
+// let types = ['num', 'alph']
+// let orders = ['asc', 'desc']
+console.log('by year - ascending')
+console.log(dynamic_sort(makes, 'num','asc')); // by year - ascending
+console.log('by year - descending');
+console.log(dynamic_sort(makes, 'num','desc')); // by year - ascending
+console.log('alphabetically - ascending');
+console.log(dynamic_sort(makes, 'alph','asc')); // alphabetically - ascending
+console.log('alphabetically - descending');
+console.log(dynamic_sort(makes, 'alph','desc')); // alphabetically - descending
+// console.log(dynamic_sort(makes, types[1], orders[0])); // alphabetically - ascending
+// console.log(dynamic_sort(makes, types[0], orders[0])); // by year - descending
+// console.log(dynamic_sort(makes, types[1], orders[0])); // alphabeticall - descending
+// console.log(dynamic_sort(makes, types[0], orders[0]).length);
+// let myArr = ['2022', '1999', undefined, '2013']
+// console.log(myArr.sort())
