7eb17e055d3475f823a1084123e2eb8cbea2a622
Andrew Derocher <am.derocher@gmail.com>
2022-01-21 21:56:01 -0800

    getting closer i hope

--- scan123.js
+++ scan123.js
@@ -14,6 +14,9 @@ let makes = [
 let types = ['num', 'alph']
 let orders = ['asc', 'desc']
 
+// accepts a string
+// if string contains a word that starts with an integer, returns integer
+// otherwise returns false
 function hasYear(string){
     let n = '0123456789'
     let itemArr = string.split(' ')
@@ -26,6 +29,8 @@ function hasYear(string){
     })
     return res
 }
+// accepts a string, returns an object
+// object.index => i, object.make => first word in string, object.year => first word starting with an int else underfined
 function createObj(string, i){
     let n = '0123456789'
     let obj = {}
@@ -42,54 +47,82 @@ function createObj(string, i){
     return obj
 }
 
+
 function dynamic_sort(arr, type, order){
-    
+    // base case
     if(arr.length <= 1){
         return arr
     }
 
-    let n = '0123456789'
-    let arrObjects = [];
+    let n = '0123456789';
+    let temp = arr[0]
+    let pivot = createObj(arr.shift())
+    let lowers = [];
+    let highers = [];
 
     arr.forEach((item, i) => {
-        splitRes.push(createObj(item, i))
+        let iObj = createObj(item, i)
+        let lesser;
+        let greater;
+        // according to the type...
+        if(type === 'num'){
+            if(iObj.year && pivot.year){
+                // find the lower year
+                if(iObj.year < pivot.year){
+                    lesser = item
+                    greater = temp
+                } else if (iObj.year >= pivot.year){ // <====== change this back to just 'greater than'
+                    lesser = temp
+                    greater = item
+                } else {
+                    let x = [iObj.make, pivot.make].sort()
+                    lesser = x[0]
+                    greater = x[1]
+                }
+            } else 
+            // if only one has year, it goes first (or second)
+            if(iObj.year || pivot.year){
+                if(iObj.year){
+                    lesser = item
+                    greater = temp
+                } else {
+                    lesser = temp
+                    greater = item
+                }
+            } else 
+            // neither have year, so go alphabetically
+            {
+
+            }
+        }
+            // then to the order...
     })
 
-    return arr.sort(function(a,b){
-        // console.log(a, b)
-        // if(hasYear(a) && hasYear(b)){
-        //     console.log('scenario a')
-        //     return a - b;
-        // } 
-        // else if (hasYear(a) || hasYear(b)) {
-        //     console.log('scenario b')
-        //     if (hasYear(a)){
-        //         return a > b 
-        //     } else {
-        //         return b > a
-        //     }
-        // }
-        // console.log('scenario c')
-        return a-b
-    })
+    return [
+        ...dynamic_sort(lowers, type, order),
+        temp,
+        ...dynamic_sort(highers, type, order)
+    ]
 
 }
-let y = [4,9,4,3,'g',8,1,5,6]
-console.log(y.sort(function(a,b){
-    if(typeof a === 'string' && typeof b !== 'string'){
-        console.log(a,b)
-        return b-a
-    }
-    if(typeof a !== 'string' && typeof b === 'string'){
-        console.log(a,b)
-        return a-b
-    } else {
-        return a-b;
-    }
-}));
 // console.log(hasYear(makes[0]))
 // console.log(hasYear(makes[1]))
 // console.log(hasYear(makes[2]))
 // console.log(hasYear(makes[3]))
 // console.log(hasYear(makes[4]))
 // console.log(dynamic_sort(makes, types[0], orders[0]));
+
+// let y = ['allison','yolanda','alex','borris','netasha']
+// console.log(['allison','yolanda','alex','borris','netasha'].sort())
+// console.log(y.sort(function(a,b){
+//     if(typeof a === 'string' && typeof b !== 'string'){
+//         console.log(a,b)
+//         return b-a
+//     }
+//     if(typeof a !== 'string' && typeof b === 'string'){
+//         console.log(a,b)
+//         return a-b
+//     } else {
+//         return a-b;
+//     }
+// }));
